eb0d118d41aada17fcd23dd857fed94071086d3c
Kiriakos <kiriakosnaiskes@gmail.com>
2017-08-07 21:13:01 +0300

    fixed spam issue

--- watchSites.go
+++ watchSites.go
@@ -7,14 +7,14 @@ import (
 )
 
 func watchSites() {
+
+	// append slice with your sites
+	// with quotation marks and commas
+	// like this: "https://url1","https://url2
+	urls := []string{}
+
 	for {
-
-		// append slice with your sites
-		// with quotation marks and commas
-		// like this: "https://url1","https://url2
-		urls := []string{}
-
-		for _, url := range urls {
+		for i, url := range urls {
 
 			resp, err := http.Get(url)
 			if err == nil {
@@ -23,8 +23,11 @@ func watchSites() {
 					// otherwise email body will not be sent
 					url := url[8:]
 					msg := url + " " + "status is" + " " +
-					strconv.Itoa(resp.StatusCode)
+						strconv.Itoa(resp.StatusCode)
 					email(msg)
+					// removing url so it will not spam
+					urls = append(urls[:i], urls[i+1:]...)
+
 				}
 				time.Sleep(10 * time.Second)
 			}
