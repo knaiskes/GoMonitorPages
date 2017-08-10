8c6fb1b389e384e0fde42c8f59ea90eff37bc87f
Kiriakos <kiriakosnaiskes@gmail.com>
2017-08-10 20:51:23 +0300

    automatically remove-add url from the list every x minutes

--- watchSites.go
+++ watchSites.go
@@ -11,26 +11,36 @@ func watchSites() {
 	// append slice with your sites
 	// with quotation marks and commas
 	// like this: "https://url1","https://url2
-	urls := []string{}
+
+	ticker := time.NewTicker(time.Millisecond)
 
 	for {
-		for i, url := range urls {
+		urls := []string{}
 
-			resp, err := http.Get(url)
-			if err == nil {
-				if resp.StatusCode != http.StatusOK {
-					// removing https from the url
-					// otherwise email body will not be sent
-					url := url[8:]
-					msg := url + " " + "status is" + " " +
-						strconv.Itoa(resp.StatusCode)
-					email(msg)
-					// removing url so it will not spam
-					urls = append(urls[:i], urls[i+1:]...)
+		go func() {
+			for {
+				for i, url := range urls {
+					time.Sleep(time.Millisecond * 1000)
 
+					resp, err := http.Get(url)
+					if err == nil {
+						if resp.StatusCode != http.StatusOK {
+							// removing https from the url
+							// otherwise email body will not be sent
+							url := url[8:]
+							msg := url + " " + "status is" + " " +
+								strconv.Itoa(resp.StatusCode)
+							email(msg)
+							// removing url so it will not spam
+							urls = append(urls[:i], urls[i+1:]...)
+
+						}
+						time.Sleep(10 * time.Second)
+					}
 				}
-				time.Sleep(10 * time.Second)
 			}
-		}
+		}()
+		time.Sleep(time.Minute * 20)
+		ticker.Stop()
 	}
 }
